ebec978a222bd2101edccda7460fd7662ef092a0
Khalil Delavaran <khalil.delavaran@gmail.com>
2026-07-09 11:07:19 +0330

    Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+
+    site: "https://fatehmusic.ir",
+
+    integrations: [
+    sitemap()
+  ],
+
   vite: {
     plugins: [tailwindcss()]
   },
