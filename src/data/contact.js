ac67219212d4ad2a43741bb515429c181fe97d31
Khalil Delavaran <khalil.delavaran@gmail.com>
2026-08-10 18:13:35 +0330

    SEO: remove map external redirects

--- src/data/contact.js
+++ src/data/contact.js
@@ -65,10 +65,10 @@ contact: {
             "https://www.google.com/maps?q=32.0415255,48.8528608",
 
         neshan:
-            "https://nshn.ir/a8_b_LH9GBh-OF",
+            "https://neshan.org/maps/share/a8_b_LH9GBh-OF",
 
         balad:
-            "https://balad.ir/p/P5GnGnNaopX9DX",
+            "https://balad.ir/p/%D8%A2%D9%85%D9%88%D8%B2%D8%B4%DA%AF%D8%A7%D9%87-%D9%85%D9%88%D8%B3%DB%8C%D9%82%DB%8C-%D9%81%D8%A7%D8%AA%D8%AD_educational-institutions-P5GnGnNaopX9DX",
 
         navigation:
             "https://www.google.com/maps?q=32.0415255,48.8528608"
