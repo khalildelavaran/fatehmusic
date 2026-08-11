80659585751879f9dfbdeb0522fab56fd079517c
Khalil Delavaran <khalil.delavaran@gmail.com>
2026-08-12 00:47:57 +0330

    Add schemaType and update contact details

--- src/data/site.js
+++ src/data/site.js
@@ -1,6 +1,13 @@
 // src/data/site.js
 
 export const site = {
+
+  // Schema
+  schemaType: [
+    "EducationalOrganization",
+    "LocalBusiness"
+  ],
+
   // General
   name: "آموزشگاه موسیقی فاتح",
   shortName: "فاتح",
@@ -46,6 +53,7 @@ export const site = {
     "آموزش نی انبان",
     "آموزش ارگ و کیبورد",
     "آموزش کمانچه",
+
     "آموزش گیتار شوشتر",
     "آموزش دف شوشتر",
     "آموزش تنبک شوشتر",
@@ -57,12 +65,8 @@ export const site = {
     "آموزش ویولن شوشتر",
     "آموزش موسیقی کودک شوشتر",
     "آموزش آواز شوشتر",
-    "آموزش آواز سنتی",
     "آموزش نی شوشتر",
-    "آموزش آواز محلی بختیاری شوشتر",
-    "آموزش آواز محلی شوشتری شوشتر",
     "آموزش آواز پاپ شوشتر",
-    "دوره ریتم و وزن خوانی شوشتر",
     "دوره سلفژ شوشتر",
     "دوره صداسازی شوشتر",
     "دوره تئوری موسیقی شوشتر",
@@ -71,24 +75,48 @@ export const site = {
     "آموزش کمانچه شوشتر"
   ],
 
+
+  // Contact
   telephone: "+986136221174",
   mobile: "+989333139319",
-
   email: "info@fatehmusic.ir",
 
+
+  // Address
   address: {
-    streetAddress: "خیابان امام غربی، پس از پاساژ مهستان، محوطه دوم پارکینگ حاج سلیمان",
+    streetAddress:
+      "خیابان امام شرقی، پس از پاساژ مهستان",
+
+    description:
+      "محوطه دوم پارکینگ حاج سلیمان",
+
     addressLocality: "شوشتر",
     addressRegion: "خوزستان",
     postalCode: "6451956156",
     addressCountry: "IR"
   },
 
+
+  // Geographic location
   geo: {
-    latitude: "32.04153",
-    longitude: "48.8554357"
+    latitude: 32.04153,
+    longitude: 48.8554357
   },
 
+
+  mapUrl:
+    "https://www.google.com/maps?q=32.04153,48.8554357",
+
+
+  // Local SEO
+  areaServed: [
+    "شوشتر",
+    "خوزستان",
+    "ایران"
+  ],
+
+
+  // Business
   openingHours: [
     "Sa-Th 16:00-21:00",
     "Fr 13:00-21:00"
@@ -96,12 +124,30 @@ export const site = {
 
   priceRange: "1,600,000",
 
+
+  // Social profiles
   socials: {
-    facebook: "https://www.facebook.com/fateh.music.academy",
-    instagram: "https://www.instagram.com/fateh.music.academy",
-    youtube: "https://www.youtube.com/@fateh.music.academy",
-    telegram: "https://t.me/fatehmusic",
-    whatsapp: "https://api.whatsapp.com/send/?phone=989333139319&text&type=phone_number&app_absent=0",
-    aparat: ""
+
+    facebook:
+      "https://www.facebook.com/fateh.music.academy",
+
+    facebookId:
+      "1302143662973158",
+
+    instagram:
+      "https://www.instagram.com/fateh.music.academy",
+
+    youtube:
+      "https://www.youtube.com/@fateh.music.academy",
+
+    telegram:
+      "https://t.me/fatehmusic",
+
+    whatsapp:
+      "https://api.whatsapp.com/send/?phone=989333139319",
+
+    aparat:
+      ""
   }
+
 };
