4818ccd665f070a0583325637b6f0ac9c2effb8d
Khalil Delavaran <khalil.delavaran@gmail.com>
2026-08-30 03:20:37 +0330

    fix(seo): link AboutPage to canonical WebPage entity

--- src/seo/schema/aboutpage.js
+++ src/seo/schema/aboutpage.js
@@ -34,9 +34,9 @@ export function buildAboutPageSchema({ title, description, image, site }) {
         isPartOf: { "@id": `${site.url}/#website` },
         about: { "@id": `${site.url}/#organization` },
         mainEntity: { "@id": `${site.url}/#organization` },
-        mainEntityOfPage: { "@type": "WebPage", "@id": url },
+        mainEntityOfPage: { "@id": `${url}#webpage` },
         primaryImageOfPage: image
-            ? { "@type": "ImageObject", url: absoluteUrl(image, site.url), caption: title, }
+            ? { "@type": "ImageObject", url: absoluteUrl(image, site.url), caption: title }
             : undefined,
         publisher: { "@id": `${site.url}/#organization` }
     });
