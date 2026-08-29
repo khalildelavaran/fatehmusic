bd770cfff15fc8e7ea4cc69f7a17762648542340
Khalil Delavaran <khalil.delavaran@gmail.com>
2026-08-30 02:59:27 +0330

    feat(seo): strengthen local organization contact signals

--- src/seo/schema/organization.js
+++ src/seo/schema/organization.js
@@ -15,156 +15,52 @@ import { SCHEMA_TYPES } from "../config/constants.js";
  * @returns {Object}
  */
 export function buildOrganizationSchema(site) {
-
   return {
-
-    "@type":
-      site.schemaType || [
-        SCHEMA_TYPES.LOCAL_EDUCATION_BUSINESS,
-        SCHEMA_TYPES.EDUCATIONAL_ORGANIZATION
-      ],
-
-
-    "@id":
-      `${site.url}/#organization`,
-
-
-    name:
-      site.name,
-
-
-    alternateName:
-      site.alternateName ||
-      site.shortName,
-
-
-    legalName:
-      site.legalName,
-
-
-    description:
-      site.description,
-
-
-    url:
-      site.url,
-
-
-    logo: {
-
-      "@type":
-        "ImageObject",
-
-      url:
-        site.logo
-
-    },
-
-
-    image:
-      site.image,
-
-
-    telephone:
-      site.telephone,
-
-
-    email:
-      site.email,
-
-
-    priceRange:
-      site.priceRange,
-
-
+    "@type": site.schemaType || [
+      SCHEMA_TYPES.LOCAL_EDUCATION_BUSINESS,
+      SCHEMA_TYPES.EDUCATIONAL_ORGANIZATION
+    ],
+    "@id": `${site.url}/#organization`,
+    name: site.name,
+    alternateName: site.alternateName || site.shortName,
+    legalName: site.legalName,
+    description: site.description,
+    url: site.url,
+    logo: { "@type": "ImageObject", url: site.logo },
+    image: site.image,
+    telephone: site.telephone,
+    email: site.email,
+    priceRange: site.priceRange,
     identifier: [
-
       site.facebookId
-        ? {
-            "@type":
-              "PropertyValue",
-
-            "propertyID":
-              "Facebook Page ID",
-
-            "value":
-              site.facebookId
-          }
+        ? { "@type": "PropertyValue", propertyID: "Facebook Page ID", value: site.facebookId }
         : undefined,
-
-
       site.googlePlaceId
-        ? {
-            "@type":
-              "PropertyValue",
-
-            "propertyID":
-              "Google Place ID",
-
-            "value":
-              site.googlePlaceId
-          }
+        ? { "@type": "PropertyValue", propertyID: "Google Place ID", value: site.googlePlaceId }
         : undefined
-
     ].filter(Boolean),
-
-
-    founder: {
-
-      "@type":
-        SCHEMA_TYPES.PERSON,
-
-      name:
-        "خلیل دلاوران"
-
-    },
-
-
+    founder: { "@type": SCHEMA_TYPES.PERSON, name: "خلیل دلاوران" },
+    contactPoint: [{
+      "@type": "ContactPoint",
+      telephone: site.telephone,
+      email: site.email,
+      contactType: "customer service",
+      areaServed: site.areaServed,
+      availableLanguage: ["fa", "fa-IR"]
+    }],
     address: {
-
-      "@type":
-        SCHEMA_TYPES.POSTAL_ADDRESS,
-
+      "@type": SCHEMA_TYPES.POSTAL_ADDRESS,
       ...site.address,
-
-
-      addressCountry:
-        site.address.addressCountry || "IR"
-
+      addressCountry: site.address.addressCountry || "IR"
     },
-
-
     geo: {
-
-      "@type":
-        SCHEMA_TYPES.GEO_COORDINATES,
-
-
-      latitude:
-        site.geo.latitude,
-
-
-      longitude:
-        site.geo.longitude
-
+      "@type": SCHEMA_TYPES.GEO_COORDINATES,
+      latitude: site.geo.latitude,
+      longitude: site.geo.longitude
     },
-
-
-    hasMap:
-      site.mapUrl,
-
-
-    areaServed:
-      site.areaServed,
-
-
-    openingHoursSpecification:
-      site.openingHoursSpecification,
-
-
-    sameAs:
-      site.sameAs
-
+    hasMap: site.mapUrl,
+    areaServed: site.areaServed,
+    openingHoursSpecification: site.openingHoursSpecification,
+    sameAs: site.sameAs
   };
-
 }
