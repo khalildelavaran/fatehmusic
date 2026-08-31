d565291eeffb3c20dfae6ffdef0a3ea8c459e0f6
Khalil Delavaran <khalil.delavaran@gmail.com>
2026-08-31 08:10:57 +0330

    feat(seo): export GSC sync services

--- src/seo/v2/providers/index.js
+++ src/seo/v2/providers/index.js
@@ -1,3 +1,4 @@
 export { createGoogleSearchConsoleClient, GSC_SCOPE } from "./search-console-client.js";
 export { getSearchConsoleStatus, testSearchConsoleConnection } from "./search-console-status.js";
 export { getSearchConsoleConfig, isSearchConsoleConfigured } from "./search-console-config.js";
+export { fetchAllSearchAnalytics, syncSearchConsoleToD1 } from "./search-console-sync.js";
