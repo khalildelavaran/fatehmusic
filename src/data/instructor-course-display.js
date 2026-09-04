cd7396baaf3db5f65a4c5e6c4c02a731e03df926
Khalil Delavaran <khalil.delavaran@gmail.com>
2026-09-04 12:23:53 +0330

    feat(data): add instructor course display overrides

---
+++ src/data/instructor-course-display.js
@@ -0,0 +1,5 @@
+export const instructorCourseDisplay = Object.freeze({
+  "khalil-delavaran": Object.freeze({
+    "guitar-course": "آموزش گیتار پاپ، آموزش گیتار کلاسیک، آموزش گیتار فلامنکو"
+  })
+});
