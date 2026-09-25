c88333ce3eed30fce75c5a4a58bbc9eb4dded639
Khalil Delavaran <khalil.delavaran@gmail.com>
2026-09-25 11:43:36 +0330

    seo: remove deprecated FAQ schema builder export

--- src/seo/index.js
+++ src/seo/index.js
@@ -19,7 +19,6 @@ import { buildPersonSchema } from "./schema/person.js";
 import { buildProfilePageSchema } from "./schema/profilePage.js";
 import { buildArticleSchema } from "./schema/article.js";
 import { buildVideoSchema } from "./schema/video.js";
-import { buildFaqSchema } from "./schema/faq.js";
 import { buildBreadcrumbSchema } from "./schema/breadcrumb.js";
 import { buildItemListSchema } from "./schema/itemlist.js";
 import { buildAboutPageSchema } from "./schema/aboutpage.js";
@@ -65,4 +64,4 @@ export function buildSEO({ path, title, description, image, canonical, noindex =
 }
 
 export { resolveSite, resolveCourse, resolveInstructor, buildCourseSchema, buildCourseStyleSchemas, buildGuitarStyleSchema, buildPersonSchema,
-    buildProfilePageSchema, buildArticleSchema, buildVideoSchema, buildFaqSchema, buildBreadcrumbSchema, buildItemListSchema, buildAboutPageSchema, buildContactPageSchema, buildLocalPlaceSchema, buildWebPageSchema, resolveTopics, topicSlugs, classifyIntent, getFreshness, buildInternalLinkPlan, buildLinkGraph, buildAnswerBlocks, answersFromFaq, buildSiteLinkCandidates, buildArticleLinkCandidates, buildContentClusterReport, buildArticleProfiles, buildArticleClusterLinks, findContentGaps, buildContentStrategy, buildUnifiedContentOpportunities, enrichOpportunitiesWithSearchConsole, buildGscSignalIndex, detectSearchCannibalization, resolveOpportunitySearchSignals, detectTemporalCannibalization, scoreOpportunity, scoreOpportunities, classifyOpportunityAction, buildSEOIntelligence, auditPage, isPrivateRoute };
+    buildProfilePageSchema, buildArticleSchema, buildVideoSchema, buildBreadcrumbSchema, buildItemListSchema, buildAboutPageSchema, buildContactPageSchema, buildLocalPlaceSchema, buildWebPageSchema, resolveTopics, topicSlugs, classifyIntent, getFreshness, buildInternalLinkPlan, buildLinkGraph, buildAnswerBlocks, answersFromFaq, buildSiteLinkCandidates, buildArticleLinkCandidates, buildContentClusterReport, buildArticleProfiles, buildArticleClusterLinks, findContentGaps, buildContentStrategy, buildUnifiedContentOpportunities, enrichOpportunitiesWithSearchConsole, buildGscSignalIndex, detectSearchCannibalization, resolveOpportunitySearchSignals, detectTemporalCannibalization, scoreOpportunity, scoreOpportunities, classifyOpportunityAction, buildSEOIntelligence, auditPage, isPrivateRoute };
